c899022d525e2c650213dd8198ee1193ce6f6f37
Novo1999 <neptune.navo@gmail.com>
2024-11-15 10:00:50 +0600

    condtion added for increment view

--- src/utils/useIncrementView.js
+++ src/utils/useIncrementView.js
@@ -1,18 +1,20 @@
 import { incrementViewCount } from '@/actions.js'
+import { useSearchParams } from 'next/navigation.js'
 import { useEffect } from 'react'
 
 const useIncrementView = () => {
-  useEffect(() => {
-    const increment = async () => {
-      try {
-        const data = await incrementViewCount()
-        return data
-      } catch (error) {
-        return null
-      }
-    }
-    if(window.location.hostname === 'localhost') return
-    increment()
-  }, [])
+    const searchParams = useSearchParams()
+    useEffect(() => {
+        const increment = async () => {
+            try {
+                const data = await incrementViewCount()
+                return data
+            } catch (error) {
+                return null
+            }
+        }
+        if (window.location.hostname === 'localhost' || searchParams.get('user') === 'novo') return;
+        increment()
+    }, [searchParams])
 }
 export default useIncrementView
